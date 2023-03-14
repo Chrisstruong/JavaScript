7d90c26b54aa0ce255bf3cd0c6646dfaf3672ec7
chrisstruong <chrisstruong@gmail.com>
2023-03-14 12:26:15 -0400

    Finish set up github

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("I like pizza!");
+console.log("Its really good!");
+window.alert("I REALLY LOVE PIZZA");
+
+// This is a comment
+/*
+    This 
+    is 
+    a multiline 
+    comment
+*/
