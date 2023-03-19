67304f696ed510620c6e1402727395db0484a2bf
chrisstruong <chrisstruong@gmail.com>
2023-03-18 20:11:42 -0400

    Finish tutorial 11, string properties

---
+++ Tutorial11/index.js
@@ -0,0 +1,18 @@
+// useful string properties & methods
+
+let userName = "    Bro Code    "
+let phoneNumber = "123-456-7890";
+// console.log(userName.length);
+
+// console.log(userName.charAt(0));
+// console.log(userName.lastIndexOf("o"));
+
+userName = userName.trim();
+userName = userName.toLowerCase()
+
+phoneNumber = phoneNumber.replaceAll('-', '/')
+
+console.log(phoneNumber)
+
+
+
