40736a651f2cae78d8596dc3a2b87e26c69b9aa1
chrisstruong <chrisstruong@gmail.com>
2023-03-18 20:26:02 -0400

    Finish tutorial 14, if statement

---
+++ Tutorial14/index.js
@@ -0,0 +1,23 @@
+// If statement = a basic form of decision making if a condition is true, then do something. If not, then dont do it!
+
+// let age = 12
+// if (age >= 18){
+//     console.log("You are an adult!")
+// } 
+// else if(age < 0){
+//     console.log("You havent been born yet!")
+// }
+// else if (age >= 65){
+//     console.log("You are a senior citizen!")
+// }
+// else {
+//     console.log("You are a child!")
+// }
+
+
+let online = true
+if (online){
+    console.log("You are online!")
+} else {
+    console.log("You are offline!")
+}
