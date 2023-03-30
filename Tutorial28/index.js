73190321793973261ce9a7a0848382c1d6dc93ee
chrisstruong <chrisstruong@gmail.com>
2023-03-30 10:12:27 -0400

    Difference between var and let variables

---
+++ Tutorial28/index.js
@@ -0,0 +1,10 @@
+// Variable scope = where a variable is accessible
+
+// let = variables are limited to block scope {}
+// var = variables are limited to a function() {}
+
+// global variable = is declared outside any function
+// (if global, var will CHANGE browser's window properties)
+
+let name = "Bro"
+
