81042eee0cd91bedfedf075ecd2244a2ff6d0937
chrisstruong <chrisstruong@gmail.com>
2023-03-30 11:25:58 -0400

    Finish array fundamentals

---
+++ Tutorial33/index.js
@@ -0,0 +1,15 @@
+// array = think of it as a variable that can store multiple values
+
+let fruits = ['apple', "orange", 'banana',]
+
+// fruits[2] = "coconut"
+
+fruits.push("lemon")//Add an element
+fruits.pop()    //removes last element
+fruits.unshift("mango") // add element to begining
+fruits.shift() //removes element from beginning
+
+let length = fruits.length
+let index= fruits.indexOf("kiwi")
+
+console.log(fruits)
