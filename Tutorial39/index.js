19dfe43a47a80b6bf19225b9ba0601ee4bd57654
chrisstruong <chrisstruong@gmail.com>
2023-03-31 13:26:28 -0400

    Work on callback

---
+++ Tutorial39/index.js
@@ -0,0 +1,16 @@
+// callback = a function passed as an argument to another function
+
+sum(2, 3, displayConsole)
+
+function sum(x, y, callback) {
+    let result = x + y
+    callback(result)
+}
+
+function displayConsole(output) {
+    console.log(output)
+}
+
+function displayDom(output) {
+    document.getElementById("myLabel").innerHTML = output
+}
