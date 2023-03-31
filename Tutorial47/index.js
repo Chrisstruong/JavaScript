a5a03aab51972f9c46d88c0d321c077d0293edfe
chrisstruong <chrisstruong@gmail.com>
2023-03-31 14:45:37 -0400

    Work on arrow function

---
+++ Tutorial47/index.js
@@ -0,0 +1,11 @@
+// arrow function = compact alternative to a traditional function expression
+// one argument, it does not require parenthesis. Two arguments needs parenthesises
+//  =>
+
+let grades = [100, 50, 90, 60, 80, 70]
+
+grades.sort(()=> y - x)
+grades.forEach((element)=> console.log(element))
+
+
+
