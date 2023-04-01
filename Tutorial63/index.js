5109a4ef2d685eeff122e5ed0029c12376dd8386
chrisstruong <chrisstruong@gmail.com>
2023-04-01 19:34:47 -0400

    Error handling

---
+++ Tutorial63/index.js
@@ -0,0 +1,24 @@
+// error = object with a description of something went wrong
+
+// Can't open a file
+// Lose connection
+// User types incorrect input
+// TypeError
+
+// throw = executes a user-defined erro
+
+// try and catch will show error, but without stoping the program.
+
+try{
+    let x = window.prompt("Enter a #")
+    x = Number(x)
+
+    if (isNaN(n)) throw "That wasn't a number!"
+    if(x == "") throw "That was empty!"
+
+    console.log(`${x} is a number`)
+} catch(error){
+    console.log(error)
+} finally{
+    console.log("This always executes!")
+}
