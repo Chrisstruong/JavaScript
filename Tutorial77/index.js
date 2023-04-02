5a8b16b249046107bb02779071211b587c6d4c91
chrisstruong <chrisstruong@gmail.com>
2023-04-02 18:54:26 -0400

    add/change html elements in js

---
+++ Tutorial77/index.js
@@ -0,0 +1,16 @@
+// add /change HTML elements
+// .innerHTML  (vulnerable to XSS attacks)
+// .textContent (more secure)
+
+// const nameTag = document.createElement("h1")
+
+// nameTag.textContent = window.prompt("Enter your name  ")
+// document.body.append(nameTag)
+
+const myList = document.querySelector("#fruit")
+const listItem = document.createElement("li")
+listItem.textContent = "mango"
+// myList.prepend(listItem)
+// myList.append(listItem)
+
+myList.insertBefore(listItem, myList.getElementsByTagName("li")[3])
