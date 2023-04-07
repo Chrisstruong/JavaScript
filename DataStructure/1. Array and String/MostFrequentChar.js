5dacc52ebe4936b06c469cbfb8a65283b1a9cda9
chrisstruong <chrisstruong@gmail.com>
2023-04-07 10:57:37 -0400

    Add Alvin's solution to mostfrequentchar

--- DataStructure/1. Array and String/MostFrequentChar.js
+++ DataStructure/1. Array and String/MostFrequentChar.js
@@ -1,23 +1,43 @@
 const mostFrequentChar = (s) => {
     // todo
+    // Alvis's solution
     const count = {}
-    let max = 0
-    const array = []
     for (let char of s){
       if (!(char in count)){
-        count[char] = 0
-      }
-      count[char] += 1
+        count[char]=0
+      } 
+      count[char] +=1
     }
     
-    for (let char in count){
-      if (max < count[char]) {
-        max = count[char]
-        array.push(char)
+    let best = null
+    for (let char of s){
+      if(best === null || count[char] > count[best]){
+        best = char
       }
     }
     
-    return array[array.length-1]
+    return best
+
+
+
+    // const count = {}
+    // let max = 0
+    // const array = []
+    // for (let char of s){
+    //   if (!(char in count)){
+    //     count[char] = 0
+    //   }
+    //   count[char] += 1
+    // }
+    
+    // for (let char in count){
+    //   if (max < count[char]) {
+    //     max = count[char]
+    //     array.push(char)
+    //   }
+    // }
+    
+    // return array[array.length-1]
   };
   
   mostFrequentChar('bookeeper')
