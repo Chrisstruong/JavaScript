484a807ed9b21cb3a8465485f205c0cae4ccf1c3
chrisstruong <chrisstruong@gmail.com>
2023-04-07 09:20:37 -0400

    Finish compress problem

---
+++ DataStructure/1. Array and String/compress.js
@@ -0,0 +1,58 @@
+const compress = (s) => {
+    // todo
+    let result = []
+    let i = 0
+    let j = 0
+    while(j <=  s.length){//have <= to have the undefined at the end of s. This is called out of bounds index
+      if(s[i] === s[j]){
+        j+=1
+      } else {
+        let num = j - i 
+        if (num === 1){
+          result.push(s[i])
+        } else{
+          result.push(num, s[i])
+        }
+        i = j 
+      }
+    }
+    
+    return result.join('')
+    
+    
+    
+    
+    
+  //   s +=""
+  //   let i = 0
+  //   let j = 0
+  //   const result = []
+  //   let tempLetter = s[0]
+  //   let count 
+    
+  //   while (j < s.length) {
+  //     if (tempLetter === s[j]){
+  //       j++
+  //     } else {
+  //       j++
+  //       count = j - i
+  //       if (count <= 1){
+  //         result.push(tempLetter)
+  //       } else {
+  //         result.push(count, tempLetter)
+  //       }
+  //       i = j
+  //       tempLetter = s[j]
+  //     }
+  //   }
+    
+  //   return result.join('')
+   
+  };
+  
+  console.log(compress('ccaaatsss'))
+  
+  module.exports = {
+    compress,
+  };
+  
