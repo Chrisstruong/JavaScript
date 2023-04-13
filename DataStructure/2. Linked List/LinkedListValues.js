363bb217575f923035eddec0539255fd98a96f6a
chrisstruong <chrisstruong@gmail.com>
2023-04-13 11:20:11 -0400

    Finish Linked List Values with rescursion solution

--- DataStructure/2. Linked List/LinkedListValues.js
+++ DataStructure/2. Linked List/LinkedListValues.js
@@ -5,20 +5,36 @@
 //   }
 // }
 
+// Iterative Solution
 //Time Complexity: O(n)
 // Space Complexity: O(n)
+// const linkedListValues = (head) => {
+//     // todo
+//     const value = []
+//     while(head!==null){
+//       value.push(head.val)
+//       head = head.next
+//     }
+
+//     return value
+//   };
+
+// Recursion Solution
+//Time Complexity: O(n)
+// Space Complexity: O(n)
+const values = []
 const linkedListValues = (head) => {
     // todo
-    const value = []
-    while(head!==null){
-      value.push(head.val)
-      head = head.next
-    }
-    
-    return value
-  };
-  
-  module.exports = {
+    fillValues(head)
+    return values
+};
+
+const fillValues = (head) => {
+    if (head === null) return
+    values.push(head.val)
+    fillValues(head.next)
+}
+
+module.exports = {
     linkedListValues,
-  };
-  
+};
