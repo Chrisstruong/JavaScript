4aebe1213dd5e8d7d33828b20988fb6205eebd69
chrisstruong <chrisstruong@gmail.com>
2023-04-16 10:12:44 -0400

    Finish get node value using recursive and iterative solutions

--- DataStructure/2. Linked List/GetNodeValue.js
+++ DataStructure/2. Linked List/GetNodeValue.js
@@ -8,15 +8,24 @@
 const getNodeValue = (head, index) => {
     // todo
     // Time: O(n)
+  //   Space: O(n)
+    if (head === null) return null
+    if (index === 0) return head.val
+    return getNodeValue(head.next, index-1)
+     
+    
+    
+    
+    // Time: O(n)
     // Space: O(1)
-    let current = head
-    let count = 0
-    while(current !== null) {
-      if(count===index) return current.val
-      count++
-      current = current.next
-    }
-    return null
+    // let current = head
+    // let count = 0
+    // while(current !== null) {
+    //   if(count===index) return current.val
+    //   count++
+    //   current = current.next
+    // }
+    // return null
   };
   
   module.exports = {
