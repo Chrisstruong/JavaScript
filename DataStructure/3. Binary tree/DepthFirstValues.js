851a6a1f7020c46c34ab2e3933adb6f63b73a5a4
chrisstruong <chrisstruong@gmail.com>
2023-05-15 12:39:18 -0400

    Finish recursive solution

--- DataStructure/3. Binary tree/DepthFirstValues.js
+++ DataStructure/3. Binary tree/DepthFirstValues.js
@@ -6,20 +6,32 @@ class Node {
     }
   }
   
-  const depthFirstValues = (root) => {
-    // to do
-    if (root === null) return []
-    const result = []
-    const stack = [ root ] // [null]
-    while(stack.length > 0) {
-      const current = stack.pop();
-      result.push(current.val)
+  // const depthFirstValues = (root) => {
+  //   // to do
+  //   if (root === null) return []
+  //   const result = []
+  //   const stack = [ root ] // [null]
+  //   while(stack.length > 0) {
+  //     const current = stack.pop();
+  //     result.push(current.val)
       
-      if(current.right) stack.push(current.right)
-      if(current.left) stack.push(current.left)
-    } 
-    return result
-  };
+  //     if(current.right) stack.push(current.right)
+  //     if(current.left) stack.push(current.left)
+  //   } 
+  //   return result
+  // };
+  
+  const depthFirstValues = (root) => {
+    if (root === null) return []
+    const leftValues = depthFirstValues(root.left)//[b, d, e]
+    const rightValues = depthFirstValues(root.right)//[c, f]
+    return [ root.val, ...leftValues, ...rightValues ]
+  }
+  
+  const peeps = ['phelipe', 'jason', 'raj', 'abby']
+  const newPeeps = ['alvin', ...peeps, 'brian']
+  console.log(newPeeps)
+  
   
   module.exports = {
     depthFirstValues,
