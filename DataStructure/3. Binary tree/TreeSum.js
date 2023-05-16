2cf6890e0303a07afde8a742f35b9579c946ca05
chrisstruong <chrisstruong@gmail.com>
2023-05-16 13:19:26 -0400

    Finish iterative solution

--- DataStructure/3. Binary tree/TreeSum.js
+++ DataStructure/3. Binary tree/TreeSum.js
@@ -8,16 +8,34 @@
 
 
 
+// const treeSum = (root) => {
+//   // todo
+//   // Recursive solution
+// // n = # nodes
+// // Time: O(n)
+// // Spae : O(n)
+//   if (root === null) return 0
+//   //        3              11                   5
+//   return root.val + treeSum(root.left) + treeSum(root.right)
+// };
+
 const treeSum = (root) => {
-    // todo
-    // Recursive solution
-  // n = # nodes
-  // Time: O(n)
-  // Spae : O(n)
+    // Iterative solution
+    // Time: O(n)
+    // Space: O(n)
     if (root === null) return 0
-    //        3              11                   5
-    return root.val + treeSum(root.left) + treeSum(root.right)
-  };
+    
+    let totalSum = 0
+    const queue = [ root ]
+    
+    while (queue.length > 0) {
+      const current = queue.shift()
+      totalSum += current.val
+      if (current.left !== null) queue.push(current.left)
+      if (current.right !== null) queue.push(current.right)
+    }
+    return totalSum
+  }
   
   module.exports = {
     treeSum,
