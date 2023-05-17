e110bfec0bc0943e47662423bed877385a18f35d
chrisstruong <chrisstruong@gmail.com>
2023-05-17 13:13:20 -0400

    Finish tree min value using breath first value

---
+++ DataStructure/3. Binary tree/TreeMinValue.js
@@ -0,0 +1,26 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const treeMinValue = (root) => {
+    // todo
+    const queue = [ root ]
+    let min = Number.MAX_VALUE
+    while (queue.length > 0) {
+      const current = queue.shift()
+      if (current.val < min) min = current.val
+      
+      if(current.left) queue.push(current.left)
+      if(current.right) queue.push(current.right)
+    }
+    return min
+  };
+  
+  module.exports = {
+    treeMinValue,
+  };
+  
