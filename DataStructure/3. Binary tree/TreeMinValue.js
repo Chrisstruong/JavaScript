272d47c830f76daf65ee683bf63ff9874083dca9
chrisstruong <chrisstruong@gmail.com>
2023-05-17 13:24:49 -0400

    Finish recursive solution

--- DataStructure/3. Binary tree/TreeMinValue.js
+++ DataStructure/3. Binary tree/TreeMinValue.js
@@ -57,6 +57,14 @@
 //   return smallest
 // }
 
+
+const treeMinValue = (root) => {
+    if (root === null) return Infinity
+    const leftMin = treeMinValue(root.left)
+    const rightMin = treeMinValue(root.right)
+    return Math.min(root.val, leftMin, rightMin)
+  }
+  
 module.exports = {
     treeMinValue,
   };
