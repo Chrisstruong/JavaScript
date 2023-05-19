b5ab8924616705fb6706a70934da86f487e8ddbe
chrisstruong <chrisstruong@gmail.com>
2023-05-19 12:32:10 -0400

    Finish iterative solution

--- DataStructure/3. Binary tree/TreeValueCount.js
+++ DataStructure/3. Binary tree/TreeValueCount.js
@@ -6,14 +6,33 @@
 //   }
 // }
 
+// const treeValueCount = (root, target) => {
+//   // todo
+//   // Recursive Solution
+//   // Time: O(n)
+//   // SpacE: O(n)
+//   if (root === null) return 0
+//   const match = root.val === target ? 1 : 0
+//   return match + treeValueCount(root.left, target) + treeValueCount(root.right, target)
+// };
+
 const treeValueCount = (root, target) => {
     // todo
-    // Recursive Solution
+    // Iterative Solution
     // Time: O(n)
-    // SpacE: O(n)
+    // Space: O(n)
+    
     if (root === null) return 0
-    const match = root.val === target ? 1 : 0
-    return match + treeValueCount(root.left, target) + treeValueCount(root.right, target)
+    
+    let count = 0
+    const queue = [root]
+    while (queue.length > 0) {
+      const current = queue.shift()
+      if (current.val === target) count++
+      if (current.left) queue.push(current.left)
+      if (current.right) queue.push(current.right)
+    }
+    return count
     
   };
   
