d38fa970bf09d2a2cf52afb596d117f3d1834f60
chrisstruong <chrisstruong@gmail.com>
2023-05-20 14:26:14 -0400

    Finish bottom right value using iterative:

---
+++ DataStructure/3. Binary tree/BottomRightValue.js
@@ -0,0 +1,31 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const bottomRightValue = (root) => {
+    // todo
+    // n = # nodes
+    // Space: O(n)
+    // Time: O(n)
+    const queue = [ root ]
+    
+    let current = null
+    while (queue.length > 0) {
+      current = queue.shift()
+      
+      if (current.left) queue.push(current.left)
+      if (current.right) queue.push(current.right)
+    }
+    
+    return current.val
+    
+  };
+  
+  module.exports = {
+    bottomRightValue,
+  };
+  
