b5a90ea30ae8ba78c6c206706a42158b0cae1600
chrisstruong <chrisstruong@gmail.com>
2023-05-22 12:38:41 -0400

    Finish leaf list using iterative

---
+++ DataStructure/3. Binary tree/LeafList.js
@@ -0,0 +1,29 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const leafList = (root) => {
+    // todo
+    // Recursive solution 
+    if (root === null) return []
+    
+    const leaves = []
+    const stack = [ root ]
+    while (stack.length > 0) {
+      const current = stack.pop()
+      if (current.left === null && current.right === null) leaves.push(current.val)
+      
+      if (current.right) stack.push(current.right)
+      if (current.left) stack.push(current.left)
+    }
+    return leaves
+  };
+  
+  module.exports = {
+    leafList,
+  };
+  
