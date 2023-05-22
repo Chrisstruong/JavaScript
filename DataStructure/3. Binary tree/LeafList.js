3a3544a5d2dcabfde14f3d128241cf0bbec7bf04
chrisstruong <chrisstruong@gmail.com>
2023-05-22 12:41:20 -0400

    add recursive

--- DataStructure/3. Binary tree/LeafList.js
+++ DataStructure/3. Binary tree/LeafList.js
@@ -7,21 +7,36 @@
 // }
 
 const leafList = (root) => {
-    // todo
-    // Recursive solution 
-    if (root === null) return []
-    
     const leaves = []
-    const stack = [ root ]
-    while (stack.length > 0) {
-      const current = stack.pop()
-      if (current.left === null && current.right === null) leaves.push(current.val)
-      
-      if (current.right) stack.push(current.right)
-      if (current.left) stack.push(current.left)
-    }
+    fillLeaves(root, leaves)
     return leaves
-  };
+  }
+  
+  const fillLeaves = (root, leaves) => {
+    if (root === null) return
+    if (root.left === null && root.right === null) leaves.push(root.val)
+    
+    fillLeaves(root.left, leaves)
+    fillLeaves(root.right, leaves)
+  }
+  
+  
+  // const leafList = (root) => {
+  //   // todo
+  //   // Iterative solution 
+  //   if (root === null) return []
+    
+  //   const leaves = []
+  //   const stack = [ root ]
+  //   while (stack.length > 0) {
+  //     const current = stack.pop()
+  //     if (current.left === null && current.right === null) leaves.push(current.val)
+      
+  //     if (current.right) stack.push(current.right)
+  //     if (current.left) stack.push(current.left)
+  //   }
+  //   return leaves
+  // };
   
   module.exports = {
     leafList,
