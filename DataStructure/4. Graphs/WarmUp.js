4e50235ecf1a8230a313bcb159d6bad97141eb86
chrisstruong <chrisstruong@gmail.com>
2023-05-23 13:11:15 -0400

    Finish Breath First Print Iteratively

--- DataStructure/4. Graphs/WarmUp.js
+++ DataStructure/4. Graphs/WarmUp.js
@@ -1,4 +1,5 @@
 // const depthFirstPrint = (graph, source) => {
+    // Iterative
 //     const stack = [ source ]
 
 //     while (stack.length > 0) {
@@ -10,15 +11,29 @@
 //         }
 //     }
 // }
-const depthFirstPrint = (graph, source) => {
-    console.log(source)
-    for (let neighbor of graph[source]) {
-        depthFirstPrint(graph, neighbor)
+
+// const depthFirstPrint = (graph, source) => {
+    // Recursive
+//     console.log(source)
+//     for (let neighbor of graph[source]) {
+//         depthFirstPrint(graph, neighbor)
+//     }
+// }
+
+const breathFirstPrint = (graph, source) => {
+    const queue = [source]
+
+    while (queue.length > 0) {
+        const current = queue.shift()
+        console.log(current)
+        for (let neighbor of graph[current]) {
+            queue.push(neighbor)
+        }
     }
 }
 
 const graph = {
-    a: ['b', 'c'],
+    a: ['c', 'b'],
     b: ['d'],
     c: ['e'],
     d: ['f'],
@@ -26,6 +41,6 @@ const graph = {
     f: []
 }
 
-depthFirstPrint(graph, 'a') //abdfce
+// depthFirstPrint(graph, 'a') //abdfce
 
-//breathFirstPrint(graph, 'a) //acbedf
+breathFirstPrint(graph, 'a') //acbedf
