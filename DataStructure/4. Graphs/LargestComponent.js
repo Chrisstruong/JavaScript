3c2723114611d9586d160c51d557145ddf908d2a
chrisstruong <chrisstruong@gmail.com>
2023-05-25 09:56:31 -0400

    Finish largest component

---
+++ DataStructure/4. Graphs/LargestComponent.js
@@ -0,0 +1,34 @@
+const largestComponent = (graph) => {
+    // todo
+    // n = # nodes
+    // e = # edges
+    // Time: O(e)
+    // Space: O(n)
+    const visited = new Set()
+    let longest = 0
+    for (let node in graph) {
+      const size = exploreSize(graph, node, visited)
+      if (size > longest) longest = size
+    }
+    return longest
+  };
+  
+  const exploreSize = (graph, node, visited) => {
+    if (visited.has(node)) return 0
+    
+    visited.add(node)
+    
+    let size = 1
+    for (let neighbor of graph[node]) {
+      size += exploreSize(graph, neighbor, visited)
+    }  
+    
+    return size
+    
+    
+  }
+  
+  module.exports = {
+    largestComponent,
+  };
+  
