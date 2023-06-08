237fcd4d4e116e4a36d9ad4d317555bb9628aae7
chrisstruong <chrisstruong@gmail.com>
2023-06-08 10:20:24 -0400

    Finish quick quickest concat with dynamic programming with memoization

---
+++ DataStructure/5. DynamicProgramming/QuickestConcat.js
@@ -0,0 +1,39 @@
+const quickestConcat = (s, words) => {
+    const answer = _quickestConcat(s, words)
+    
+    if (answer === Infinity) {
+      return -1
+    } else {
+      return answer
+    }
+  }
+  
+  const _quickestConcat = (s, words, memo = {}) => {
+    // todo
+    // Dymamoc Programming w/ Memoization
+    // s = string length
+    // n = # words
+    // Time: O(ns)
+    // Space: O(s)
+    
+    if (s in memo) return memo[s]
+    
+    if (s === '') return 0
+    
+    let minWords = Infinity ;
+    for (let word of words) {
+      if (s.startsWith(word)){
+        const suffix = s.slice(word.length)
+        const attempt = 1 + _quickestConcat(suffix, words, memo);
+        minWords = Math.min(attempt, minWords)
+      }
+    }
+    
+    memo[s] = minWords
+    return minWords
+  };
+  
+  module.exports = {
+    quickestConcat,
+  };
+  
