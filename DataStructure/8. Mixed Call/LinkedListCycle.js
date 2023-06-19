8516c2818834247dd2520a3a188db26520c66434
chrisstruong <chrisstruong@gmail.com>
2023-06-19 11:23:02 -0400

    add better solution

--- DataStructure/8. Mixed Call/LinkedListCycle.js
+++ DataStructure/8. Mixed Call/LinkedListCycle.js
@@ -5,22 +5,41 @@
 //   }
 // }
 
+// const linkedListCycle = (head) => {
+//   // todo
+//   // n = # nodes
+//   // Time: O(n)
+//   // Space: O(1)
+//   const nodes = new Set()
+//   let current = head
+  
+//   while (current !== null) {
+//     if (nodes.has(current)) return true
+//     nodes.add(current)
+//     current = current.next
+//   }
+  
+//   return false
+// };
+
 const linkedListCycle = (head) => {
-    // todo
+  
     // n = # nodes
     // Time: O(n)
-    // Space: O(n)
-    const nodes = new Set()
-    let current = head
+    // Space: O(1)
+    let slow = head
+    let fast = head
+    let firstIteration = true
     
-    while (current !== null) {
-      if (nodes.has(current)) return true
-      nodes.add(current)
-      current = current.next
+    while (fast !== null && fast.next !== null) {
+      if (fast === slow && ! firstIteration) return true
+      fast = fast.next.next
+      slow = slow.next
+      firstIteration = false
     }
     
     return false
-  };
+  }
   
   module.exports = {
     linkedListCycle,
