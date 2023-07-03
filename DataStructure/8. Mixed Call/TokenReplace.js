aa129e97350321de410bd1bb82894e4ff49994f7
chrisstruong <chrisstruong@gmail.com>
2023-07-03 12:47:57 -0400

    Change output to an array and then join

--- DataStructure/8. Mixed Call/TokenReplace.js
+++ DataStructure/8. Mixed Call/TokenReplace.js
@@ -3,24 +3,24 @@ const tokenReplace = (s, tokens) => {
     // n = length of string
     // Time: O(n)
     // Space: O(n)
-    let output = ''
+    let output = []
     let i = 0
     let j = 1
     while (i < s.length) {
       if (s[i] !== '$') {
-        output += s[i]
+        output.push(s[i])
         i += 1
         j = i + 1
       } else if (s[j] !== '$') {
         j += 1
       } else {
         const key = s.slice(i, j + 1)
-        output += tokens[key]
+        output.push(tokens[key])
         i = j + 1
         j = i + 1
       } 
     }
-      return output
+      return output.join('')
   };
   
   const tokens = {
