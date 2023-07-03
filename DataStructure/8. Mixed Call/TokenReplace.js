ab736ec5b4680e794804d8a0df0ada979821f0c7
chrisstruong <chrisstruong@gmail.com>
2023-07-03 12:46:01 -0400

    Finish token replace

---
+++ DataStructure/8. Mixed Call/TokenReplace.js
@@ -0,0 +1,36 @@
+const tokenReplace = (s, tokens) => {
+    // todo
+    // n = length of string
+    // Time: O(n)
+    // Space: O(n)
+    let output = ''
+    let i = 0
+    let j = 1
+    while (i < s.length) {
+      if (s[i] !== '$') {
+        output += s[i]
+        i += 1
+        j = i + 1
+      } else if (s[j] !== '$') {
+        j += 1
+      } else {
+        const key = s.slice(i, j + 1)
+        output += tokens[key]
+        i = j + 1
+        j = i + 1
+      } 
+    }
+      return output
+  };
+  
+  const tokens = {
+    '$LOCATION$': 'park',
+    '$ANIMAL$': 'dog',
+  };
+  tokenReplace('Walk the $ANIMAL$ in the $LOCATION$!', tokens); 
+  // -> 'Walk the dog in the park!'
+  
+  module.exports = {
+    tokenReplace,
+  };
+  
