36ff0c846bddbe4d8df67cb041438c376c86372d
chrisstruong <chrisstruong@gmail.com>
2023-07-08 12:50:47 -0400

    Finish Instance of

---
+++ DataStructure/FreeCodeCamp/OOP/InstanceOf.js
@@ -0,0 +1,9 @@
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
+  
+  const myHouse = new House(10)
+  
+  myHouse instanceof House
+  
+  // Only change code below this line
