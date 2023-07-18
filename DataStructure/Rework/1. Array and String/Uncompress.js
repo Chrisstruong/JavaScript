705fbc600494bfbc72e8c7a408ada47716ee2433
chrisstruong <chrisstruong@gmail.com>
2023-07-18 09:16:00 -0400

    Finish Uncompress

---
+++ DataStructure/Rework/1. Array and String/Uncompress.js
@@ -0,0 +1,34 @@
+// Two pointer Strategy 
+const uncompress = (s) => {
+    // todo
+    
+    
+  //   push element to the array always have constant time of time complexity
+  //   n is the number of groups : ex"2c, 300a, 5f"
+  //   M is the maximum number of each group: ex: "2, 300, 5"
+  //   Time complexity: O(nM)
+  //   Space complexity: O(nM)
+    
+    const numbers = "0123456789"
+    let result = []
+    let i = 0
+    let j = 0
+    while (j < s.length) {
+      if (numbers.includes(s[j])) {
+        j += 1
+      } else {
+        const num = Number(s.slice(i, j))
+        for (let count = 0; count < num; count += 1) {
+          result.push(s[j])
+        } 
+        j += 1
+        i = j
+      }
+    }
+    return result.join('')
+  };
+  
+  module.exports = {
+    uncompress,
+  };
+  
