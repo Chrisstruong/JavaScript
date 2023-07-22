214216bbb0bfbb2a2f35a7747a8fcfe14c508350
chrisstruong <chrisstruong@gmail.com>
2023-07-22 08:45:04 -0400

    Finish pair product

---
+++ DataStructure/Rework/1. Array and String/PairProduct.js
@@ -0,0 +1,29 @@
+const pairProduct = (numbers, targetProduct) => {
+    // todo
+    // Hash map solution
+    // Time: O(n)
+  //   Space: O(n)
+    const previousNum = {}
+    for (let i = 0; i < numbers.length; i++) {
+      const num = numbers[i]
+      const complement = targetProduct / num
+      if (complement in previousNum) return [i, previousNum[complement]]
+      previousNum[num] = i
+    }
+    
+    //Brute force (nested loop) solution:
+    // Time: O(n^2)
+    // Space: O(1)
+    // for (let i = 0; i < numbers.length; i++){
+    //   for (let j = i + 1; j < numbers.length; j++){
+    //     if (numbers[i] * numbers[j] === targetProduct){
+    //       return [i,j]
+    //     }
+    //   }
+    // }
+  };
+  
+  module.exports = {
+    pairProduct,
+  };
+  
