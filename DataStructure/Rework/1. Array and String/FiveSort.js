81dd471f5d06c352841731b027aa2d556a667667
chrisstruong <chrisstruong@gmail.com>
2023-07-24 09:10:30 -0400

    Finish five sort

---
+++ DataStructure/Rework/1. Array and String/FiveSort.js
@@ -0,0 +1,26 @@
+const fiveSort = (nums) => {
+    // todo
+    // n = array length
+    // Time: O(n), Space O(1)
+    // using two pointers
+     let i = 0
+     let j = nums.length - 1
+     while (i < j) {
+       if (nums[j] === 5) {
+         j -= 1
+       } else if (nums[i] === 5) {
+         [ nums[i], nums[j] ] = [ nums[j], nums[i] ] // this is how you swap position in array
+         i += 1
+       } else i += 1
+     }
+    return nums
+     
+    
+  };
+  
+  // [1,2,3,5,3,3,5]
+  
+  module.exports = {
+    fiveSort,
+  };
+  
