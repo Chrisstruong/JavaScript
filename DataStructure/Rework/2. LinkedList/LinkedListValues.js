d8e66ee1753d22dd4333d5ca4c059080d2fa959b
chrisstruong <chrisstruong@gmail.com>
2023-07-25 09:44:28 -0400

    Finish Linked List based on solution on Structy

--- DataStructure/Rework/2. LinkedList/LinkedListValues.js
+++ DataStructure/Rework/2. LinkedList/LinkedListValues.js
@@ -1,53 +1,55 @@
 class Node {
-    constructor(val) {
-      this.val = val;
-      this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  
-  //Time Complexity: O(n)
-  // Space Complexity: O(n)
-  // const linkedListValues = (head) => {
-  //   // todo
-  //   const values = []
-  //   let current = head
-  //   while(current !== null) {
-  //     values.push(current.val)
-  //     current = current.next
-  //   }
-  //   return values
-  // };
-  
-  // Iterative style: Using while loop 
-  // Recursive style: Uisng recursive call
+}
+
+//Time Complexity: O(n)
+// Space Complexity: O(n)
+// const linkedListValues = (head) => {
+//   // todo
+//   const values = []
+//   let current = head
+//   while(current !== null) {
+//     values.push(current.val)
+//     current = current.next
+//   }
+//   return values
+// };
+
+// Iterative style: Using while loop 
+// Recursive style: Uisng recursive call
+const linkedListValues = (head) => {
+  // todo
   const result = []
-  const linkedListValues = (head) => {
-    // todo
-    // if (head === null) {return result}
-    //console.log(`result: ${result}`)
-    if (head.val === "b") return result
-    result.push(head.val)
-    head = head.next
-    linkedListValues(head)
-    
-  };
-  const a = new Node("a");
-  const b = new Node("b");
-  const c = new Node("c");
-  const d = new Node("d");
+  _linkedListValues(head, result)
+  return result
   
-  a.next = b;
-  b.next = c;
-  c.next = d;
-  
-  // a -> b -> c -> d
-  
-  console.log(linkedListValues(a)); // -> [ 'a', 'b', 'c', 'd' ]
-  
-  
-  
-  
-  module.exports = {
-    linkedListValues,
-  };
-  
+};
+const _linkedListValues = (head, values) => {
+  // todo
+ if (head === null) return values
+ values.push(head.val)
+ head = head.next
+  _linkedListValues(head, values)
+};
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(linkedListValues(a)); // -> [ 'a', 'b', 'c', 'd' ]
+
+
+
+
+module.exports = {
+  linkedListValues,
+};
