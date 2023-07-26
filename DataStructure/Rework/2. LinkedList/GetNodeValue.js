4d88fa55a62ac4c4a23f6a15a10a292c8cc5bfda
chrisstruong <chrisstruong@gmail.com>
2023-07-26 08:19:01 -0400

    Finish get node value iteratively

---
+++ DataStructure/Rework/2. LinkedList/GetNodeValue.js
@@ -0,0 +1,47 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// Iterative Stylle
+const getNodeValue = (head, index) => {
+    let current = head
+    let count = 0
+    while (current !== null) {
+      if (count === index) return current.val
+      count ++
+      current = current.next
+    }
+    return null
+  }
+  
+  
+  // const getNodeValue = (head, index) => {
+  //   // todo
+  //   // Time: O(n)
+  // //   Space: O(n)
+  //   if (head === null) return null
+  //   if (index === 0) return head.val
+  //   return getNodeValue(head.next, index-1)
+     
+    
+    
+    
+  //   // Time: O(n)
+  //   // Space: O(1)
+  //   // let current = head
+  //   // let count = 0
+  //   // while(current !== null) {
+  //   //   if(count===index) return current.val
+  //   //   count++
+  //   //   current = current.next
+  //   // }
+  //   // return null
+  // };
+  
+  module.exports = {
+    getNodeValue,
+  };
+  
