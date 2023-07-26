f0d497051f6ebbbf1ddf0be794531a9c24412725
chrisstruong <chrisstruong@gmail.com>
2023-07-26 08:14:00 -0400

    Finish linkedlist find iteratively

---
+++ DataStructure/Rework/2. LinkedList/LinkedListFind.js
@@ -0,0 +1,48 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// Iterative style
+const linkedListFind = (head, target) => {
+    let current = head
+    while (current !== null) {
+      if (current.val === target) return true
+      current = current.next
+    }
+    return false
+  }
+  
+  
+  // Iterative solution
+  // const linkedListFind = (head, target) => {
+  //   // todo
+  //   //Time: O(n)
+  //   //Space: O(1)
+  //   let current = head
+  //   while (current !== null){
+  //     if (target === current.val) return true
+  //     current = current.next
+  //   }
+  //   return false
+  // };
+  
+  // const a = new Node("a");
+  // const b = new Node("b");
+  // const c = new Node("c");
+  // const d = new Node("d");
+  
+  // a.next = b;
+  // b.next = c;
+  // c.next = d;
+  
+  // a -> b -> c -> d
+  
+  // linkedListFind(a, "c");
+  
+  module.exports = {
+    linkedListFind,
+  };
+  
