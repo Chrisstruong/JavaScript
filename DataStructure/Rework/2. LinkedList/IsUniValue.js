b41a6dfd8cd8fa83e13b0bb66607b95c118125f7
chrisstruong <chrisstruong@gmail.com>
2023-08-01 12:34:38 -0400

    Update recursive solution

--- DataStructure/Rework/2. LinkedList/IsUniValue.js
+++ DataStructure/Rework/2. LinkedList/IsUniValue.js
@@ -1,20 +1,38 @@
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
-// }
-
-// Iterative solution
-const isUnivalueList = (head) => {
-    let prev = head.val
-    while (head !== null) {
-      if (head.val !== prev) return false
-      head = head.next
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
     }
-    return true
-    
   }
+  
+  // Recursive Style
+  const isUnivalueList = (head, prev = head.val) => {
+    console.log(prev)
+    if (head === null) return true
+    if (head.val !== prev) return false
+    return isUnivalueList(head.next, prev)
+  }
+  const a = new Node(7);
+  const b = new Node(7);
+  const c = new Node(4);
+  
+  a.next = b;
+  b.next = c;
+  
+  // 7 -> 7 -> 4
+  
+  isUnivalueList(a);
+  
+  // Iterative solution
+  // const isUnivalueList = (head) => {
+  //   let prev = head.val
+  //   while (head !== null) {
+  //     if (head.val !== prev) return false
+  //     head = head.next
+  //   }
+  //   return true
+    
+  // }
   
   
   
