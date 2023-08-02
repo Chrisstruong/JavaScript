9197fa1ed28eca0130916169262a851bf1da6b4e
chrisstruong <chrisstruong@gmail.com>
2023-08-02 11:33:15 -0400

    Finish Remove Node recursively

--- DataStructure/Rework/2. LinkedList/RemoveNode.js
+++ DataStructure/Rework/2. LinkedList/RemoveNode.js
@@ -6,32 +6,40 @@
 // }
 
 
-
-
-
-// const removeNode = (head, targetVal) => {
-//   // Iterative style
-//   // n = # of nodes
-//   // Time: O(n)
-//   // Space: O(1)
-//   if (head.val === targetVal) {
-//     return head.next
-//   }
-//   let prev = null
-//   let current = head
+const removeNode = (head, targetVal) => {
+    if (head === null) return null
+    
+    if (head.val === targetVal) return head.next
+    
+    head.next = removeNode(head.next, targetVal)
+    return head
+  }
+  // a -> b -> c -> d -> e -> f
   
-//   while (current !== null) {
-//     if (current.val === targetVal) {
-//       prev.next = current.next
-//       break;
-//     }
-//     prev = current
-//     current = current.next
-//   }
-//   return head
-// }
-
-module.exports = {
+  
+  // const removeNode = (head, targetVal) => {
+  //   // Iterative style
+  //   // n = # of nodes
+  //   // Time: O(n)
+  //   // Space: O(1)
+  //   if (head.val === targetVal) {
+  //     return head.next
+  //   }
+  //   let prev = null
+  //   let current = head
+    
+  //   while (current !== null) {
+  //     if (current.val === targetVal) {
+  //       prev.next = current.next
+  //       break;
+  //     }
+  //     prev = current
+  //     current = current.next
+  //   }
+  //   return head
+  // }
+  
+  module.exports = {
     removeNode,
   };
   
