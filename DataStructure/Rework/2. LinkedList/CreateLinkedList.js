2c1997b7dc53c8e37f39cf9a5f588d0f2b6f4e78
chrisstruong <chrisstruong@gmail.com>
2023-08-03 11:56:40 -0400

    Update recursive style to this solution

--- DataStructure/Rework/2. LinkedList/CreateLinkedList.js
+++ DataStructure/Rework/2. LinkedList/CreateLinkedList.js
@@ -5,19 +5,30 @@ class Node {
     }
   }
   
-  // Iterative Style
+  // Recursive style version1
   const createLinkedList = (values) => {
     // Complexity
-    // Time: O(n)
+    // Time: O(n^2)
     // Space: O(n)
-    const dummyHead = new Node(null)
-    let tail = dummyHead
-    for (let val of values) {
-      tail.next = new Node(val)
-      tail = tail.next
-    }
-    return dummyHead.next
-  }; 
+    if (values.length === 0) return null
+    const head = new Node(values[0])
+    head.next = createLinkedList(values.slice(1))
+    return head
+  }
+  
+  // Iterative Style
+  // const createLinkedList = (values) => {
+  //   // Complexity
+  //   // Time: O(n)
+  //   // Space: O(n)
+  //   const dummyHead = new Node(null)
+  //   let tail = dummyHead
+  //   for (let val of values) {
+  //     tail.next = new Node(val)
+  //     tail = tail.next
+  //   }
+  //   return dummyHead.next
+  // }; 
   
   
   createLinkedList(["h", "e", "y"]);
