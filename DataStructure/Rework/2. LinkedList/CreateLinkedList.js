7d8e0dd6216346e8c158f7a12335eabdbabffd2a
chrisstruong <chrisstruong@gmail.com>
2023-08-03 12:00:46 -0400

    Update second version of recursion

--- DataStructure/Rework/2. LinkedList/CreateLinkedList.js
+++ DataStructure/Rework/2. LinkedList/CreateLinkedList.js
@@ -5,16 +5,32 @@ class Node {
     }
   }
   
-  // Recursive style version1
-  const createLinkedList = (values) => {
+  // // Recursive style version1
+  // const createLinkedList = (values) => {
+  //   // Complexity
+  //   // Time: O(n^2)
+  //   // Space: O(n)
+  //   if (values.length === 0) return null
+  //   const head = new Node(values[0])
+  //   head.next = createLinkedList(values.slice(1))
+  //   return head
+  // }
+  
+  // Recursive style version 2
+  const createLinkedList = (values, i = 0) => {
     // Complexity
-    // Time: O(n^2)
+    // Time: O(n)
     // Space: O(n)
-    if (values.length === 0) return null
-    const head = new Node(values[0])
-    head.next = createLinkedList(values.slice(1))
+    if (i === values.length) return null
+    const head = new Node(values[i])
+    head.next = createLinkedList(values, i + 1)
     return head
   }
+  
+  //   0    1    2
+  // ["h", "e", "y"]
+  // (h) -> (e) -> (y) -> null
+  
   
   // Iterative Style
   // const createLinkedList = (values) => {
